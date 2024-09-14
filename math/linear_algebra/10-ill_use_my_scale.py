320361f7f51cb4a65af971a9ee4c81f8d3552677
Bfestus <f.bigiriman@alustudent.com>
2024-09-14 23:27:29 +0200

    maths

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+This Script calculate shapes of matrices.
+
+"""
+
+
+def np_shape(matrix):
+    """
+    Calculate the shape of a numpy.ndarray.
+
+    Args:
+        matrix (numpy.ndarray): The input matrix.
+
+    Returns:
+        tuple: The shape of the matrix as a tuple of integers.
+    """
+    return matrix.shape
---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""
+This script transposes matrix
+
+"""
+
+
+def np_transpose(matrix):
+    """
+    Transposes a numpy.ndarray.
+
+    Args:
+        matrix: A numpy.ndarray.
+
+    Returns:
+        A new numpy.ndarray which is the transpose of the input matrix.
+    """
+    # Return the transpose of the input matrix
+    return matrix.transpose()
